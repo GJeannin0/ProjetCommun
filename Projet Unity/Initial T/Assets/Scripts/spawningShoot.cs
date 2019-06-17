64c4be735f9b97eee0a9f3d6d5c59cb89c173898
Tristan <tristan.schorr@hotmail.fr>
2019-06-17 19:54:43 +0200

    add black marks for tank and shoot, and drift feeling a bit better

---
+++ Projet Unity/Initial T/Assets/Scripts/spawningShoot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawningShoot : MonoBehaviour
+{
+    [SerializeField] private Transform spawner;
+    [SerializeField] private GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("y"))
+            Instantiate(bulletPrefab, spawner.position, spawner.rotation);
+    }
+}
